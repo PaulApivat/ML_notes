17f2c7ae5cb7fac31ca99bc419faa16fd4a957c7
paulapivat <paul.apivat@gmail.com>
2020-10-11 13:30:31 +0700

    clean up bmi-api/app.py

--- aiat/wk4_docker/bmi-api/App.py
+++ aiat/wk4_docker/bmi-api/App.py
@@ -1,30 +1,3 @@
-Last login: Sat Oct 10 13:34:52 on ttys001
-(base) paulapivat@Pauls-MacBook ~ % ls
-2018			Music			data_baby_boy_j_j.png
-Applications		Pictures		data_science_R
-Desktop			Postman			diet_data
-Documents		Public			diet_data.zip
-Downloads		Rplot.png		opt
-Dropbox			Rprof.out		scikit_learn_data
-Library			Zotero			testdir
-Movies			baby names.pdf
-(base) paulapivat@Pauls-MacBook ~ % cd Desktop/RCode/ML_notes
-(base) paulapivat@Pauls-MacBook ML_notes % ls
-R		README.md	aiat		hands_on_ml
-(base) paulapivat@Pauls-MacBook ML_notes % cd aiat
-(base) paulapivat@Pauls-MacBook aiat % ls
-python				wk2_hypothesis_testing
-wk2_bagging_ensemble_methods	wk3_homework
-wk2_classical_ml		wk3_tensor_flow
-wk2_eval_measure		wk4_docker
-wk2_homework			wk4_superai_api
-(base) paulapivat@Pauls-MacBook aiat % cd wk4_docker
-(base) paulapivat@Pauls-MacBook wk4_docker % ls
-bmi-api			node-bulletin-board	test
-(base) paulapivat@Pauls-MacBook wk4_docker % cd bmi-api
-(base) paulapivat@Pauls-MacBook bmi-api % nano App.py
-
-  GNU nano 2.0.6               File: App.py                           Modified  
 
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
@@ -44,7 +17,3 @@ if __name__ == '__main__':
 
 
 
-
-
-^G Get Help  ^O WriteOut  ^R Read File ^Y Prev Page ^K Cut Text  ^C Cur Pos
-^X Exit      ^J Justify   ^W Where Is  ^V Next Page ^U UnCut Text^T To Spell
